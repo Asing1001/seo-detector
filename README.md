ef2625b1082be043c77aa519ba061c8c91ab33ab
Andy Chen <shinningstar1001@gmail.com>
2018-04-15 17:45:51 +0800

    add tests for writeconsole

--- README.md
+++ README.md
@@ -18,3 +18,32 @@
 
 1. Detect if a HTML have more than one `<H1>` tag.
 
+## Development Requirement
+
+1. This package should be production ready and a NPM module
+
+1. User is free to chain any rules by themselves
+
+    I. For example, they can only use the rule 1 and 4 or only use rule 2. 
+
+    II. The order of rules is doesn’t matter
+
+1. User can define and use their own rules easily
+
+1. The input can be:
+
+    I. A HTML file (User is able to config the input path)
+
+    II. Node Readable Stream
+
+1. The output can be:
+
+    I. A file (User is able to config the output destination)
+
+    II. Node Writable Stream
+
+    III. Console
+
+1. Your package should be flexible: 
+
+    I. When we want to implement additional rules for `<meta>` tag, The code changes should be small. Ex: Checking `<meta name=“robots” />` existing or not?!
--- test/detector.test.js
+++ test/detector.test.js
@@ -1,7 +1,90 @@
-import detector from "../"
-import test from 'ava';
+const path = require('path');
+const detector = require("../");
+const test = require('ava');
+const mock = require('mock-fs');
+const sinon = require('sinon');
+const stream = require('stream');
+const cheerio = require('cheerio');
+const input = require('../lib/input')
 
-test('detector', t => {
-    const seoDetector = new detector();
-    t.pass();
+let sandbox = sinon.sandbox.create();
+const filePath = "path/to/file.html";
+const fileContent = "foo";
+
+test.beforeEach(async t => {
+    sandbox = sinon.sandbox.create();
+    const fileSystem = {
+        [filePath]: fileContent
+    }
+
+    mock(fileSystem)
+})
+
+test.afterEach(async t => {
+    sandbox.restore();
+    mock.restore();
+})
+
+test('detector writeConsole should call console.log', async t => {
+    const logSpy = sandbox.spy(console, 'log');
+    const seoDetector = new detector({
+        input: filePath,
+        rules: [],
+    });
+
+    await seoDetector.writeConsole()
+    t.true(logSpy.called)
 });
+
+test('input filename without exception', async t => {
+    const seoDetector = new detector({
+        input: filePath,
+        rules: [],
+    });
+
+    await seoDetector.writeConsole()
+    t.pass()
+});
+
+test('input readable stream without exception', async t => {
+    const seoDetector = new detector({
+        input: new stream.Readable(),
+        rules: [],
+    });
+
+    await seoDetector.writeConsole()
+    t.pass()
+});
+
+test('customRule.validate should be called with cheerio object', async t => {
+    const customRule = { validate: sandbox.spy() };
+
+
+    const seoDetector = new detector({
+        input: "path/to/file.html",
+        rules: [customRule],
+    });
+
+    await seoDetector.writeConsole()
+    t.true(customRule.validate.calledWith(cheerio.load(fileContent)))
+});
+
+// test('detector getWritable should return writable stream', async t => {
+//     // const rule = {
+//     //     validate: $ => $('strong').count() > 15,
+//     // }
+
+//     mock({
+//         'some-file.html': ''//new Buffer([8, 6, 7, 5, 3, 0, 9])
+//     });
+
+//     const seoDetector = new detector({
+//         input: filePath,
+//         rules: [],
+//     });
+
+//     // await seoDetector.getWritable();
+//     // await seoDetector.writeConsole()
+//     // await seoDetector.writeFile()
+//     t.pass();
+// });
