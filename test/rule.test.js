842e05ce89c8dc5e571facfed264291b6fce07a1
Andy Chen <shinningstar1001@gmail.com>
2018-04-17 23:53:53 +0800

    Add strong limit

--- test/rule.test.js
+++ test/rule.test.js
@@ -40,14 +40,14 @@ test('anchorShouldWithRel', async t => {
     t.deepEqual(actual, [`There are 1 <a> tag without rel attribute`])
 });
 
-test('<head>: no <head> no error', async t => {
+test('head: no <head> no error', async t => {
     const actual = rule.preDefined.head.validate(
         cheerio.load(
             `<body></body>`))
     t.deepEqual(actual, [])
 });
 
-test('<head>: has <title>, <meta name="descriptions"/>, <meta name="keywords"/> no error', async t => {
+test('head: has <title>, <meta name="descriptions"/>, <meta name="keywords"/> no error', async t => {
     const actual = rule.preDefined.head.validate(
         cheerio.load(
             `<head>
@@ -58,7 +58,7 @@ test('<head>: has <title>, <meta name="descriptions"/>, <meta name="keywords"/> 
     t.deepEqual(actual, [])
 });
 
-test('<head>: without <title> have an error', async t => {
+test('head: without <title> have an error', async t => {
     const actual = rule.preDefined.head.validate(
         cheerio.load(
             `<head>
@@ -68,7 +68,7 @@ test('<head>: without <title> have an error', async t => {
     t.deepEqual(actual, ["This html without <title> tag"])
 });
 
-test('<head>: without <meta name="descriptions"/> have an error', async t => {
+test('head: without <meta name="descriptions"/> have an error', async t => {
     const actual = rule.preDefined.head.validate(
         cheerio.load(
             `<head>
@@ -78,7 +78,7 @@ test('<head>: without <meta name="descriptions"/> have an error', async t => {
     t.deepEqual(actual, [`This html without <meta name="descriptions"> tag`])
 });
 
-test('<head>: without <meta name="keywords"/> have an error', async t => {
+test('head: without <meta name="keywords"/> have an error', async t => {
     const actual = rule.preDefined.head.validate(
         cheerio.load(
             `<head>
@@ -86,4 +86,41 @@ test('<head>: without <meta name="keywords"/> have an error', async t => {
             <meta name="descriptions">            
             </head>`))
     t.deepEqual(actual, [`This html without <meta name="keywords"> tag`])
+});
+
+test('strongLimit(): should have function validate', async t => {
+    const limitRule = rule.preDefined.strongLimit();
+    t.is(typeof limitRule.validate, 'function')    
+});
+
+test('strongLimit(0): no <strong> no error ', async t => {
+    const limit = 0;
+    const actual = rule.preDefined.strongLimit(limit).validate(
+        cheerio.load(
+            `<body>
+            </body>`), limit)
+    t.deepEqual(actual, [])
+});
+
+test('strongLimit(0): 1 <strong> have error ', async t => {
+    const limit = 0;
+    const actual = rule.preDefined.strongLimit(limit).validate(
+        cheerio.load(
+            `<body>
+            <strong>1</strong>
+            </body>`), limit)
+    t.deepEqual(actual, [`There are more than ${limit} strong tag`])
+});
+
+test('strongLimit(15): 16 <strong> have error ', async t => {
+    const limit = 15;
+    const actual = rule.preDefined.strongLimit(limit).validate(
+        cheerio.load(
+            `<body>
+            <strong></strong><strong></strong><strong></strong><strong></strong><strong></strong>
+            <strong></strong><strong></strong><strong></strong><strong></strong><strong></strong>
+            <strong></strong><strong></strong><strong></strong><strong></strong><strong></strong>
+            <strong></strong>
+            </body>`), limit)
+    t.deepEqual(actual, [`There are more than ${limit} strong tag`])
 });
