105d6978873f576761837da27633c05cfeb416d4
Andy Chen <shinningstar1001@gmail.com>
2018-04-17 10:21:04 +0800

    add tests for img with alt

--- test/rule.test.js
+++ test/rule.test.js
@@ -2,24 +2,40 @@ const cheerio = require('cheerio');
 const { test } = require('ava');
 const { rule } = require("../");
 
-test('tagWithAttr', async t => {
-    const tagShouldWithAttribute = rule.tagWithAttr('tagName', 'attributeName');
-    const actual = tagShouldWithAttribute.validate(
-        cheerio.load(
-            `<tagName foo /> <tagName bar />`));
-    t.true(actual === `There are 2 <tagName> tag without attributeName attribute`)
-});
-
-test('imgShouldWithAlt', async t => {
+test('imgShouldWithAlt: no img output no errors', async t => {
     const actual = rule.preDefined.imgShouldWithAlt.validate(
         cheerio.load(
-            `<img src="foo" alt="alt" /> <img src="foo" />`));
-    t.true(actual === `There are 1 <img> tag without alt attribute`)
+            `<h1></h1>`));
+    t.deepEqual(actual, []);
+});
+
+test('imgShouldWithAlt: img with alt output no errors', async t => {
+    const actual = rule.preDefined.imgShouldWithAlt.validate(
+        cheerio.load(
+            `<img src="foo" alt="alt" />
+            <img src="foo" alt="bar"/>`));
+    t.deepEqual(actual, [])
+});
+
+test('imgShouldWithAlt: img without alt output one error', async t => {
+    const actual = rule.preDefined.imgShouldWithAlt.validate(
+        cheerio.load(
+            `<img src="foo" />`));
+    t.is(actual.length, 1);
+});
+
+test('imgShouldWithAlt: two img without alt output one error with count 2', async t => {
+    const actual = rule.preDefined.imgShouldWithAlt.validate(
+        cheerio.load(
+            `<img src="foo" />
+            <img src="foo" />`));
+    t.deepEqual(actual, [`There are 2 <img> tag without alt attribute`])
 });
 
 test('anchorShouldWithRel', async t => {
     const actual = rule.preDefined.anchorShouldWithRel.validate(
         cheerio.load(
-            `<a rel="url" /> <a />`))
-    t.true(actual === `There are 1 <a> tag without rel attribute`)
+            `<a rel="url"></a>
+            <a></a>`))
+    t.deepEqual(actual, [`There are 1 <a> tag without rel attribute`])
 });
